9f1213e6a3560511529aab7b6d3760819d62d5b5
DeDTihoN <tihonmarkp@gmail.com>
2023-12-19 16:21:55 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿using MaterialSkin.Controls;
+
+namespace CityTemperature
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CityNameBox = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.ResultButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.ResultLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // CityNameBox
+            // 
+            this.CityNameBox.Depth = 0;
+            this.CityNameBox.Hint = "";
+            this.CityNameBox.Location = new System.Drawing.Point(107, 79);
+            this.CityNameBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.CityNameBox.Name = "CityNameBox";
+            this.CityNameBox.PasswordChar = '\0';
+            this.CityNameBox.SelectedText = "";
+            this.CityNameBox.SelectionLength = 0;
+            this.CityNameBox.SelectionStart = 0;
+            this.CityNameBox.Size = new System.Drawing.Size(166, 23);
+            this.CityNameBox.TabIndex = 0;
+            this.CityNameBox.Text = "Введите город";
+            this.CityNameBox.UseSystemPasswordChar = false;
+            this.CityNameBox.Click += new System.EventHandler(this.CityNameBox_Click);
+            this.CityNameBox.Enter += new System.EventHandler(this.CityNameBox_Enter);
+            this.CityNameBox.Leave += new System.EventHandler(this.CityNameBox_Leave);
+            // 
+            // ResultButton
+            // 
+            this.ResultButton.Depth = 0;
+            this.ResultButton.Location = new System.Drawing.Point(132, 127);
+            this.ResultButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ResultButton.Name = "ResultButton";
+            this.ResultButton.Primary = true;
+            this.ResultButton.Size = new System.Drawing.Size(124, 52);
+            this.ResultButton.TabIndex = 1;
+            this.ResultButton.Text = "получить текущую температуру";
+            this.ResultButton.UseVisualStyleBackColor = true;
+            this.ResultButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Depth = 0;
+            this.ResultLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.ResultLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ResultLabel.Location = new System.Drawing.Point(12, 202);
+            this.ResultLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 19);
+            this.ResultLabel.TabIndex = 2;
+            this.ResultLabel.Click += new System.EventHandler(this.ResultLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 272);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.ResultButton);
+            this.Controls.Add(this.CityNameBox);
+            this.Name = "Form1";
+            this.Text = "Температура города";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSingleLineTextField CityNameBox;
+        private MaterialRaisedButton ResultButton;
+        private MaterialLabel ResultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Google.Cloud.Translation.V2;
+using Newtonsoft.Json.Linq;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace CityTemperature
+{
+    public partial class Form1 : MaterialForm
+    {
+        private const string GoogleTranslateApiKey = "AIzaSyAqbcpU-ZU0K_krumb6wlijpKzud3Jpej4";
+        private const string OpenWeatherMapApiKey = "76b6a91eaa1f27a40f0bff71376e1b55";
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Teal500, Primary.Teal700, Primary.Teal500, Accent.DeepOrange700, TextShade.WHITE);
+        }
+
+
+        // функция перевода города на английский язык с помощью Google Translate API 
+        private string TranslateCityToEnglish(string russianCity)
+        {
+            TranslationClient client = TranslationClient.CreateFromApiKey(GoogleTranslateApiKey);
+
+            var response = client.TranslateText(russianCity, "en", "ru");
+            return response.TranslatedText;
+        }
+
+
+        // функция получения температуры в городе с помощью OpenWeatherMap API 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string russianCity = CityNameBox.Text;
+            string englishCity = TranslateCityToEnglish(russianCity);
+            try
+            {
+                string apiKey = OpenWeatherMapApiKey;
+                string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={englishCity}&units=metric&appid={apiKey}";
+
+
+                // получение данных API запроса с помощью библиотеки WebClient 
+                using (WebClient client = new WebClient())
+                {
+                    string json = client.DownloadString(apiUrl);
+                    // парсинг данных с помощью библиотеки Newtonsoft.Json 
+                    JObject data = JObject.Parse(json);
+                    if (data["main"] != null && data["main"]["temp"] != null)
+                    {
+                        double temperature = Convert.ToDouble(data["main"]["temp"]);
+                        ResultLabel.Text = $"Текущая температура в городе {russianCity}: {temperature} C°";
+                    }
+                    else
+                    {
+                        ResultLabel.Text = "Не удалось получить данные";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultLabel.Text = "Не удалось получить данные\n\t\t\t( возможно введено неправильное название города)";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CityNameBox_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void CityNameBox_Leave(object sender, EventArgs e)
+        {
+        }
+
+        private void CityNameBox_Click(object sender, EventArgs e)
+        {
+            if (CityNameBox.Text == "Введите город")
+                CityNameBox.Clear();
+        }
+    }
+}
